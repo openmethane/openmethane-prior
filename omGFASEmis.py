38519135ea883f5f845dcbafb5a7eb547d67d6f9
Jared Lewis <jared@jared.kiwi.nz>
2024-04-16 11:43:31 +1000

    chore: Add license header to the files

--- omGFASEmis.py
+++ omGFASEmis.py
@@ -1,11 +1,27 @@
-"""
-omGFASEmis.py
+#
+# Copyright 2024 The Superpower Institute Ltd.
+#
+# This file is part of OpenMethane.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-Copyright 2023 The Superpower Institute Ltd
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
-You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
+"""
+Download and process GFAS data
+
+This downloads files from [ADS](https://atmosphere.copernicus.eu/data).
+See the project readme for more information about configuring
+the required credentials.
 """
 
 import numpy as np
