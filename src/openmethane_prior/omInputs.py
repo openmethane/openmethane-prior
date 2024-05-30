1438b24b08c875fe7909e931d148ee4bcdd57ae8
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2024-05-30 12:02:49 +1000

    Add projection origin to domain projection

--- src/openmethane_prior/omInputs.py
+++ src/openmethane_prior/omInputs.py
@@ -74,7 +74,17 @@ domainProj = None
 if os.path.exists(domainPath):
     with xr.open_dataset(domainPath) as dss:
         domainXr = dss.load()
-        domainProj = pyproj.Proj(proj='lcc', lat_1=domainXr.TRUELAT1, lat_2=domainXr.TRUELAT2, lat_0=domainXr.MOAD_CEN_LAT, lon_0=domainXr.STAND_LON, a=6370000, b=6370000)
+        domainProj = pyproj.Proj(
+            proj='lcc',
+            lat_1=domainXr.TRUELAT1,
+            lat_2=domainXr.TRUELAT2,
+            lat_0=domainXr.MOAD_CEN_LAT,
+            lon_0=domainXr.STAND_LON,
+            x_0=domainXr.XORIG,
+            y_0=domainXr.YORIG,
+            a=6370000,
+            b=6370000,
+        )
 
 def checkInputFile(file, errorMsg, errors):
     ## Check that all required input files are present
