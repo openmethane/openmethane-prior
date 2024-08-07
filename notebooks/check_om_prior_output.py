485da64f092721525ff02f62fc63dc59afad095e
Daniel Busch <danielbusch@climate-resource.com>
2024-08-07 12:24:06 +0200

    min max

--- notebooks/check_om_prior_output.py
+++ notebooks/check_om_prior_output.py
@@ -1,6 +1,25 @@
 import xarray as xr
+from matplotlib import pyplot as plt
+import pandas as pd
 
 output_file = "../data/outputs/out-om-domain-info.nc"
 
-xr.open_dataset(output_file)
+ds = xr.open_dataset(output_file)
+
+
+
+
+ds.LANDMASK.plot()
+
+# print min and max values for all variables
+for var in ds:
+    arr = ds[var].squeeze().to_numpy()
+    print(f"{var}: MIN {arr.min():.2e}, MAX {arr.max():.2e}")
+
+arr[arr < 0]
+
+
+
+
+
 
