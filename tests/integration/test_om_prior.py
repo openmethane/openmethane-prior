077a29dd35b0d100520abe220436493e670d9847
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2025-11-18 14:57:08 +1100

    Create a single snapshots for test_009_prior_emissions_ds
    Originally, this test makes several assertions against snapshots of expected scientific results. However, when changes are made to the codebase that cause expected changes, this test had to be run multiple times (very slowly) to discover and fix the changes to each different comparison.
    
    This change gathers all the calculations for the result into a single large snapshot, which can be checked in a single assertion. When values change, the entire new snapshot can be used to replace the old.

--- tests/integration/test_om_prior.py
+++ tests/integration/test_om_prior.py
@@ -4,6 +4,7 @@ import pytest
 
 from openmethane_prior.sectors.fire.sector import gfas_data_source
 from openmethane_prior.lib.sector.config import PriorSectorConfig
+from testing import dataset_metrics
 
 
 @pytest.mark.skip(reason="Duplicated by other tests")
@@ -17,111 +18,98 @@ def test_002_cdsapi_connection(tmp_path, sector_config: PriorSectorConfig):
 
 
 def test_009_prior_emissions_ds(prior_emissions_ds):
-    numeric_keys = [key for key in prior_emissions_ds.keys() if np.issubdtype(prior_emissions_ds[key].dtype, np.number)]
-
-    result_means = {key: prior_emissions_ds[key].mean().item() for key in numeric_keys}
-    expected_means = {
-        "lambert_conformal": 0.0,
-        "land_mask": 1.0,
-        "lat": -23.267749786376953,
-        "lon": 148.6399383544922,
-        "x_bounds": 1530000.375,
-        "y_bounds": 364369.5,
-
-        "ch4_sector_agriculture": 1.8375328756935794e-12,
-        "ch4_sector_coal": 4.250515218652202e-10,
-        "ch4_sector_lulucf": 7.412380485305677e-12,
-        "ch4_sector_waste": 5.9131867104723875e-12,
-        "ch4_sector_livestock": 4.0547154335803873e-11,
-        "ch4_sector_industrial": 5.645377828854301e-14,
-        "ch4_sector_stationary": 8.9951585930315e-13,
-        "ch4_sector_transport": 2.0972791286013642e-13,
-        "ch4_sector_electricity": 2.283888848991229e-13,
-        "ch4_sector_oil_gas": 0.0,
-        "ch4_sector_termite": 2.436579367090519e-12,
-        "ch4_sector_fire": 3.6974516872713414e-13,
-        "ch4_sector_wetlands": 1.2524275273123607e-10,
-        "ch4_total": 6.102049399521322e-10,
-
-        # deprecated
-        "OCH4_TOTAL": 6.102049399521322e-10,
-        "LANDMASK": 1.0,
+    assert dataset_metrics(prior_emissions_ds) == {
+        'max': {
+            'LANDMASK': 1.0,
+            'OCH4_TOTAL': 1.3572649472123647e-08,
+            'ch4_sector_agriculture': 2.3277985923203306e-12,
+            'ch4_sector_coal': 1.3467229766109225e-08,
+            'ch4_sector_electricity': 1.284687477557566e-11,
+            'ch4_sector_fire': 5.498670963000052e-11,
+            'ch4_sector_industrial': 5.463926537135979e-13,
+            'ch4_sector_livestock': 7.545056510007907e-11,
+            'ch4_sector_lulucf': 1.3240418166971984e-10,
+            'ch4_sector_oil_gas': 0.0,
+            'ch4_sector_stationary': 8.706040097264147e-12,
+            'ch4_sector_termite': 3.3858020553889645e-12,
+            'ch4_sector_transport': 2.0298692902317285e-12,
+            'ch4_sector_waste': 1.8478708470226208e-10,
+            'ch4_sector_wetlands': 2.5512281176531815e-10,
+            'ch4_total': 1.3572649472123647e-08,
+            'lambert_conformal': 0,
+            'land_mask': 1,
+            'lat': -22.806066513061523,
+            'lon': 149.1439208984375,
+            'x_bounds': 1580000.375,
+            'y_bounds': 414369.5,
+        },
+        'mean': {
+            'LANDMASK': 1.0,
+            'OCH4_TOTAL': 6.102049399521322e-10,
+            'ch4_sector_agriculture': 1.8375328756935794e-12,
+            'ch4_sector_coal': 4.250515218652202e-10,
+            'ch4_sector_electricity': 2.283888848991229e-13,
+            'ch4_sector_fire': 3.6974516872713414e-13,
+            'ch4_sector_industrial': 5.645377828854301e-14,
+            'ch4_sector_livestock': 4.0547154335803873e-11,
+            'ch4_sector_lulucf': 7.412380485305677e-12,
+            'ch4_sector_oil_gas': 0.0,
+            'ch4_sector_stationary': 8.9951585930315e-13,
+            'ch4_sector_termite': 2.436579367090519e-12,
+            'ch4_sector_transport': 2.0972791286013642e-13,
+            'ch4_sector_waste': 5.9131867104723875e-12,
+            'ch4_sector_wetlands': 1.2524275273123607e-10,
+            'ch4_total': 6.102049399521322e-10,
+            'lambert_conformal': 0.0,
+            'land_mask': 1.0,
+            'lat': -23.267749786376953,
+            'lon': 148.6399383544922,
+            'x_bounds': 1530000.375,
+            'y_bounds': 364369.5,
+        },
+        'x_band': {
+            'LANDMASK': 10.0,
+            'OCH4_TOTAL': 2.3336199335565213e-09,
+            'ch4_sector_agriculture': 4.2241433895661604e-11,
+            'ch4_sector_coal': 0.0,
+            'ch4_sector_electricity': 0.0,
+            'ch4_sector_fire': 0.0,
+            'ch4_sector_industrial': 1.0547281076660498e-12,
+            'ch4_sector_livestock': 8.812359129868445e-10,
+            'ch4_sector_lulucf': 4.0341018755540166e-11,
+            'ch4_sector_oil_gas': 0.0,
+            'ch4_sector_stationary': 1.6805689341982532e-11,
+            'ch4_sector_termite': 4.7307820855158056e-11,
+            'ch4_sector_transport': 3.9183546498005505e-12,
+            'ch4_sector_waste': 0.0,
+            'ch4_sector_wetlands': 1.3007149732291445e-09,
+            'ch4_total': 2.3336199335565213e-09,
+            'land_mask': 10,
+            'lat': -232.6233367919922,
+            'lon': 1486.8963623046875,
+        },
+        'y_band': {
+            'LANDMASK': 10.0,
+            'OCH4_TOTAL': 1.629799852568423e-08,
+            'ch4_sector_agriculture': 4.089383399587878e-11,
+            'ch4_sector_coal': 1.2910974623596018e-08,
+            'ch4_sector_electricity': 0.0,
+            'ch4_sector_fire': 0.0,
+            'ch4_sector_industrial': 3.778536261999508e-13,
+            'ch4_sector_livestock': 1.0031707194660715e-09,
+            'ch4_sector_lulucf': 0.0,
+            'ch4_sector_oil_gas': 0.0,
+            'ch4_sector_stationary': 6.020594893132918e-12,
+            'ch4_sector_termite': 4.639714307197451e-11,
+            'ch4_sector_transport': 1.403740454438857e-12,
+            'ch4_sector_waste': 0.0,
+            'ch4_sector_wetlands': 2.288760014845792e-09,
+            'ch4_total': 1.629799852568423e-08,
+            'land_mask': 10,
+            'lat': -232.2213897705078,
+            'lon': 1486.336669921875,
+        },
     }
-
-    assert result_means == expected_means
-
-    result_maxs = {key: prior_emissions_ds[key].max().item() for key in numeric_keys}
-    expected_maxs = {
-        "lambert_conformal": 0,
-        "land_mask": 1,
-        "lat": -22.806066513061523,
-        "lon": 149.1439208984375,
-        "x_bounds": 1580000.375,
-        "y_bounds": 414369.5,
-
-        "ch4_sector_agriculture": 2.3277985923203306e-12,
-        "ch4_sector_coal": 1.3467229766109225e-08,
-        "ch4_sector_lulucf": 1.3240418166971984e-10,
-        "ch4_sector_waste": 1.8478708470226208e-10,
-        "ch4_sector_livestock": 7.545056510007907e-11,
-        "ch4_sector_industrial": 5.463926537135979e-13,
-        "ch4_sector_stationary": 8.706040097264147e-12,
-        "ch4_sector_transport": 2.0298692902317285e-12,
-        "ch4_sector_electricity": 1.284687477557566e-11,
-        "ch4_sector_oil_gas": 0.0,
-        "ch4_sector_termite": 3.3858020553889645e-12,
-        "ch4_sector_fire": 5.498670963000052e-11,
-        "ch4_sector_wetlands": 2.5512281176531815e-10,
-        "ch4_total": 1.3572649472123647e-08,
-
-        # deprecated
-        "OCH4_TOTAL": 1.3572649472123647e-08,
-        "LANDMASK": 1.0,
-    }
-
-    assert result_maxs == expected_maxs
-
-    # spot check an entire row and column to ensure no geometric shifting
-    results_keys = [key for key in prior_emissions_ds.keys() if key.startswith("ch4")]
-    result_y_band = {key: prior_emissions_ds[key][0, 0, 4].sum().item() for key in results_keys}
-    expected_y_band = {
-        "ch4_sector_agriculture": 2.0089220728243947e-11,
-        "ch4_sector_coal": 0.0,
-        "ch4_sector_lulucf": 0.0,
-        "ch4_sector_waste": 0.0,
-        "ch4_sector_livestock": 4.0390212981728717e-10,
-        "ch4_sector_industrial": 8.447339161268685e-13,
-        "ch4_sector_stationary": 1.3459711244899672e-11,
-        "ch4_sector_transport": 3.1382183181070133e-12,
-        "ch4_sector_electricity": 0.0,
-        "ch4_sector_oil_gas": 0.0,
-        "ch4_sector_termite": 2.3266076565331417e-11,
-        "ch4_sector_fire": 0.0,
-        "ch4_sector_wetlands": 1.118552889201041e-09,
-        "ch4_total": 1.5832529808752393e-09,
-    }
-
-    assert result_y_band == expected_y_band
-
-    result_x_band = {key: prior_emissions_ds[key][0, 0, :, 4].sum().item() for key in results_keys}
-    expected_x_band = {
-        "ch4_sector_agriculture": 1.9864620744946812e-11,
-        "ch4_sector_coal": 5.180719352568863e-09,
-        "ch4_sector_lulucf": 6.118763459290032e-11,
-        "ch4_sector_waste": 0.0,
-        "ch4_sector_livestock": 4.7123459295837e-10,
-        "ch4_sector_industrial": 6.323611496026155e-13,
-        "ch4_sector_stationary": 1.0075833719532698e-11,
-        "ch4_sector_transport": 2.3492454907470443e-12,
-        "ch4_sector_electricity": 1.284687477557566e-11,
-        "ch4_sector_oil_gas": 0.0,
-        "ch4_sector_termite": 2.363145790162946e-11,
-        "ch4_sector_fire": 0.0,
-        "ch4_sector_wetlands": 6.705475445734077e-10,
-        "ch4_total": 6.453089517825054e-09,
-    }
-
-    assert result_x_band == expected_x_band
 
 
 def test_011_output_dims(prior_emissions_ds):
