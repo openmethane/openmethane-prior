19a954c06c0a74fa77f02025f507ec720d7845a0
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2025-11-27 15:44:48 +1100

    Add method for allocating Safeguard facility emissions to oil field shapes
    The allocate_safeguard_facility_emissions method in the oil_gas sector module is similar to the implementation in the coal module, except it works on a data source which provides geometry shapes for locations instead of lat/lon points.
    
    In this naive implementation the emissions from a single facility are spread out equally over the area covered by its locations. Two grid cells fully covered by a facility location would be attributed equal emissions, while grid cells with only partial coverage would get a smaller share of the facility's reported total.

---
+++ src/openmethane_prior/sectors/oil_gas/safeguard_oil_gas.py
@@ -0,0 +1,81 @@
+#
+# Copyright 2025 The Superpower Institute Ltd.
+#
+# This file is part of OpenMethane.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import numpy as np
+
+from openmethane_prior.data_sources.safeguard import (
+    filter_facilities,
+    filter_locations,
+    get_safeguard_facility_locations,
+)
+from openmethane_prior.lib import (
+    convert_to_timescale,
+    DataAsset,
+    logger,
+    polygon_cell_intersection,
+    PriorConfig,
+)
+
+logger = logger.get_logger(__name__)
+
+def allocate_safeguard_facility_emissions(
+    config: PriorConfig,
+    anzsic_codes: list[str],
+    safeguard_facilities_asset: DataAsset,
+    facility_locations_asset: DataAsset,
+    reference_data_asset: DataAsset,
+):
+    domain_grid = config.domain_grid()
+
+    sector_facilities = filter_facilities(
+        facility_df=safeguard_facilities_asset.data,
+        anzsic_codes=anzsic_codes,
+        period=(config.start_date.date(), config.end_date.date()),
+    )
+
+    oil_gas_facilities, oil_gas_locations_pivot = get_safeguard_facility_locations(
+        safeguard_facilities_df=sector_facilities,
+        locations_df=facility_locations_asset.data,
+        data_source_name=reference_data_asset.name,
+    )
+
+    oil_gas_locations = reference_data_asset.data
+    oil_gas_facilities_locations = oil_gas_locations.merge(
+        oil_gas_locations_pivot,
+        right_on="data_source_id",
+        left_on="id",
+    )
+
+    # make an empty grid to allocate emissions to
+    gridded_annual_emissions = np.zeros(config.domain_grid().shape)
+
+    for _, facility in oil_gas_facilities.iterrows():
+        facility_locations = filter_locations(oil_gas_facilities_locations, facility_id=facility["facility_name"])
+
+        # use the union of all facility location shapes, which will prevent
+        # overlapping shapes from being double-counted
+        facility_all_areas = facility_locations["geometry"].union_all()
+        facility_cells = polygon_cell_intersection(facility_all_areas, domain_grid)
+
+        for cell_indexes, area_proportion in facility_cells:
+            # allocate a fraction of the facility emissions to each cell
+            # based on the portion of the total area in each cell
+            gridded_annual_emissions[cell_indexes[1], cell_indexes[0]] += facility["ch4_kg"] * area_proportion
+
+    gridded_emissions = convert_to_timescale(gridded_annual_emissions, config.domain_grid().cell_area)
+
+    return oil_gas_facilities, oil_gas_facilities_locations, gridded_emissions
---
+++ tests/integration/test_sector_oil_gas/test_allocate_safeguard.py
@@ -0,0 +1,70 @@
+
+import attrs
+import datetime
+
+import numpy as np
+
+from openmethane_prior.data_sources.safeguard import safeguard_mechanism_data_source, safeguard_locations_data_source
+from openmethane_prior.lib import PriorConfig
+from openmethane_prior.lib.grid.geometry import polygon_cell_intersection
+from openmethane_prior.sectors.oil_gas.data import vic_oil_gas_data_source
+from openmethane_prior.sectors.oil_gas.safeguard_oil_gas import allocate_safeguard_facility_emissions
+
+
+def test_oil_gas_allocate_safeguard(config: PriorConfig, data_manager):
+    # we will need to test with configs in the SGM period and outside
+    config_params = attrs.asdict(config)
+    del config_params["start_date"]
+    del config_params["end_date"]
+
+    # period within safeguard period, using the full domain since test fields
+    # are in Victoria, so not in au-test
+    config_2023 = PriorConfig(
+        **config_params,
+        domain_path="https://openmethane.s3.amazonaws.com/domains/aust10km/v1/domain.aust10km.nc",
+        start_date=datetime.datetime(2023, 7, 1),
+        end_date=datetime.datetime(2023, 7, 2),
+    )
+
+    safeguard_facilities_asset = data_manager.get_asset(safeguard_mechanism_data_source)
+    facility_locations_asset = data_manager.get_asset(safeguard_locations_data_source)
+    oil_gas_asset = data_manager.get_asset(vic_oil_gas_data_source)
+
+    # verify facility emissions in safeguard data
+    otway_facility = safeguard_facilities_asset.data[safeguard_facilities_asset.data["facility_name"] == "Otway"]
+    assert len(otway_facility) == 1
+    assert otway_facility.iloc[0]["ch4_kg"] == 257750.0
+
+    # verify facilities related to Otway lie inside the domain
+    oil_gas_otway_fields = oil_gas_asset.data[oil_gas_asset.data["tag"].isin(["geographe", "thylacine"])]
+    assert len(oil_gas_otway_fields) == 2
+    oil_gas_otway_geometry = oil_gas_otway_fields["geometry"].union_all()
+    oil_gas_otway_cells = polygon_cell_intersection(oil_gas_otway_geometry, config_2023.domain_grid())
+    assert oil_gas_otway_cells == [
+        ((309, 84), 0.049298356711897674),
+        ((308, 85), 0.1448363451138687),
+        ((309, 85), 0.40146462148077633),
+        ((309, 86), 0.40120661630811183),
+        ((310, 86), 0.003194060385346512),
+    ]
+
+    # run the test
+    facilities, locations, gridded_emissions = allocate_safeguard_facility_emissions(
+        config=config_2023,
+        anzsic_codes=["070"],
+        safeguard_facilities_asset=safeguard_facilities_asset,
+        facility_locations_asset=facility_locations_asset,
+        reference_data_asset=oil_gas_asset,
+    )
+
+    assert len(facilities) == 1
+    assert len(locations) == 2
+
+    for cell_indexes, area_proportion in oil_gas_otway_cells:
+        ix, iy = cell_indexes
+        expected_emissions = (area_proportion * 257750.0
+                              / config_2023.domain_grid().cell_area
+                              / (365 * 24 * 60 * 60))
+
+        # check the emissions are allocated to the right grid cell
+        assert np.isclose(gridded_emissions[iy, ix], expected_emissions)
