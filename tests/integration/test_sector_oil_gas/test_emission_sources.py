51292da070302724bff1b063dc4397f3e0f8b8d4
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2026-03-16 08:54:17 +1100

    Add NSW oil & gas emission sources from petroleum drillholes
    Combine NSW drillholes dataset with petroleum leases dataset to extract producing wells with an activity period matching the period of interest.

--- tests/integration/test_sector_oil_gas/test_emission_sources.py
+++ tests/integration/test_sector_oil_gas/test_emission_sources.py
@@ -2,10 +2,39 @@ import datetime
 
 from openmethane_prior.sectors.oil_gas.data.nopta_titles import nopta_titles_data_source
 from openmethane_prior.sectors.oil_gas.data.nopta_wells import nopta_wells_data_source
+from openmethane_prior.sectors.oil_gas.data.nsw_drillholes import nsw_drillholes_data_source
+from openmethane_prior.sectors.oil_gas.data.nsw_titles import nsw_titles_data_source
 from openmethane_prior.sectors.oil_gas.data.qld_boreholes import qld_boreholes_data_source
 from openmethane_prior.sectors.oil_gas.data.qld_leases import qld_leases_data_source
+from openmethane_prior.sectors.oil_gas.emission_sources.nsw_sources import nsw_emission_sources
 from openmethane_prior.sectors.oil_gas.emission_sources.offshore_sources import offshore_emission_sources
 from openmethane_prior.sectors.oil_gas.emission_sources.qld_sources import qld_emission_sources
+
+
+def test_nsw_emission_sources(input_files, data_manager):
+    start_date = datetime.datetime(2023, 1, 1, 0, 0)
+    start_date_end = datetime.datetime(2023, 1, 2, 0, 0)
+    drillholes_da = data_manager.get_asset(nsw_drillholes_data_source)
+    df = nsw_emission_sources(
+        start_date=start_date.date(),
+        end_date=start_date.date(),
+        nsw_drillholes_da=drillholes_da,
+        nsw_titles_da=data_manager.get_asset(nsw_titles_data_source),
+    )
+
+    # original drillholes dataset has been filtered down
+    assert len(drillholes_da.data) == 775
+    assert len(df) == 92
+
+    # no sources where activity period doesn't intersect date period
+    assert len(df[(df["activity_end"] < start_date) & (df["activity_start"] > start_date_end)]) == 0
+
+    # no sources which aren't related to production
+    assert list(df["hole_purpose"].unique()) == ["Production"]
+    assert list(df["resource"].unique()) == ["PETROLEUM"]
+
+    # no duplicate entries for the same location
+    assert len(df["geometry"].unique()) == len(df)
 
 
 def test_qld_emission_sources(input_files, data_manager):
