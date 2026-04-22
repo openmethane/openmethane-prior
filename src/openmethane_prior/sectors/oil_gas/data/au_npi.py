f59cc7d38f88d2f2e2770ddfb3db2143c7dd9ed7
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2026-04-22 18:14:35 +1000

    Add NPI facilities to oil and gas emission sources

--- src/openmethane_prior/sectors/oil_gas/data/au_npi.py
+++ src/openmethane_prior/sectors/oil_gas/data/au_npi.py
@@ -23,27 +23,27 @@ from openmethane_prior.lib import DataSource
 from openmethane_prior.lib.data_manager.source import ConfiguredDataSource
 
 
-def parse_oil_gas_sites_csv(data_source: ConfiguredDataSource) -> gpd.GeoDataFrame:
+def parse_au_npi_csv(data_source: ConfiguredDataSource) -> gpd.GeoDataFrame:
     """Read and parse the oil-gas-processing CSV asset, and convert to a
     GeoDataFrame using the latitude/longitude columns."""
     df = pd.read_csv(
         data_source.asset_path,
         converters={
-            "Operation start": np.datetime64,
-            "Operation end": np.datetime64,
+            # ensure codes like "0700" are not turned into numeric 700
+            "primary_anzsic_class_code": str,
         },
     )
 
     # remove entries without lat/lon, these aren't useful to us
-    df = df[(~np.isnan(df["Longitude"])) & (~np.isnan(df["Latitude"]))]
+    df = df[(~np.isnan(df["longitude"])) & (~np.isnan(df["latitude"]))]
 
     gdf = gpd.GeoDataFrame(
         data=df,
-        geometry=gpd.points_from_xy(df["Longitude"], df["Latitude"]),
+        geometry=gpd.points_from_xy(df["longitude"], df["latitude"]),
         crs="EPSG:4326",
     )
-    del gdf["Longitude"]
-    del gdf["Latitude"]
+    del gdf["longitude"]
+    del gdf["latitude"]
 
     gdf = gdf.to_crs(data_source.prior_config.crs)
 
@@ -53,8 +53,8 @@ def parse_oil_gas_sites_csv(data_source: ConfiguredDataSource) -> gpd.GeoDataFra
 # Processing facilities in the oil and gas industry in Australia. This dataset
 # was manually created from research conducted by The Superpower Institute to
 # identify locations of processing sites linked to Safeguard Mechanism facilities.
-oil_gas_sites_data_source = DataSource(
-    name="oil-gas-sites",
-    url="https://openmethane.s3.amazonaws.com/prior/inputs/oil-gas-sites-v0.3.csv",
-    parse=parse_oil_gas_sites_csv,
+au_npi_data_source = DataSource(
+    name="AU-NPI",
+    url="https://data.gov.au/data/dataset/043f58e0-a188-4458-b61c-04e5b540aea4/resource/f83cdee9-ebcb-4f24-941b-34bb2f0996cf/download/facilities.csv",
+    parse=parse_au_npi_csv,
 )
