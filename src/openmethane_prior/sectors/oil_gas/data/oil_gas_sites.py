00b5ce99be40109a845032b7cc1467ebdaae999c
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2026-04-24 16:22:30 +1000

    Fetch oil and gas sites data directly from Google Sheets
    This dataset is maintained and kept up to date in Google Sheets. Previously, after each update or revision the sheet would be manually exported to CSV, saved, and uploaded to the Open Methane Public Data Store.
    
    This obviates that workflow by fetching data directly from a publicly viewable Google Sheet which fetches data from the original via Google Sheets' IMPORTRANGE() function.

--- src/openmethane_prior/sectors/oil_gas/data/oil_gas_sites.py
+++ src/openmethane_prior/sectors/oil_gas/data/oil_gas_sites.py
@@ -18,9 +18,46 @@
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+import pathlib
+import urllib.parse
 
 from openmethane_prior.lib import DataSource
 from openmethane_prior.lib.data_manager.source import ConfiguredDataSource
+
+
+def fetch_google_sheet_csv(
+    sheet_id_or_url: str,
+    sheet_name: str,
+) -> pd.DataFrame:
+    """Fetch a single sheet from a publicly viewable Google Sheet and return
+    the contents as a pandas DataFrame."""
+    if "://docs.google.com" in sheet_id_or_url:
+        # if a url is provided, extract the sheet_id
+        sheet_url = urllib.parse.urlparse(sheet_id_or_url)
+        sheet_id = sheet_url.path.rpartition('/')[-1]
+    else:
+        sheet_id = sheet_id_or_url
+
+    # remove strings from the sheet name so it is url-safe
+    sheet_name = sheet_name.replace(" ", "")
+
+    # Google Sheets provides a CSV endpoint for downloading
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&amp;sheet={sheet_name}"
+    df = pd.read_csv(url)
+
+    # remove empty columns with no name or data
+    for column_name in df.columns:
+        if "Unnamed: " in column_name and df[column_name].isnull().all():
+            del df[column_name]
+
+    return df
+
+
+def fetch_oil_gas_sites_csv(data_source: ConfiguredDataSource) -> pathlib.Path:
+    """Fetch the oil and gas sites from Google Sheets and save as a CSV."""
+    df = fetch_google_sheet_csv(data_source.url, "Oil and gas sites")
+    df.to_csv(data_source.asset_path)
+    return data_source.asset_path
 
 
 def parse_oil_gas_sites_csv(data_source: ConfiguredDataSource) -> gpd.GeoDataFrame:
@@ -50,11 +87,14 @@ def parse_oil_gas_sites_csv(data_source: ConfiguredDataSource) -> gpd.GeoDataFra
     return gdf
 
 
-# Processing facilities in the oil and gas industry in Australia. This dataset
-# was manually created from research conducted by The Superpower Institute to
-# identify locations of processing sites linked to Safeguard Mechanism facilities.
+# Processing facilities in the oil and gas industry in Australia. This dataset,
+# created by The Superpower Institute, identifies locations of oil and gas
+# sites linked to Safeguard Mechanism facilities.
+# Source: https://docs.google.com/spreadsheets/d/1vET6DVXo3K9MeMYJj9sksSTmQjV3v9JmIPSlR6HS4NA
 oil_gas_sites_data_source = DataSource(
     name="oil-gas-sites",
-    url="https://openmethane.s3.amazonaws.com/prior/inputs/oil-gas-sites-v0.3.csv",
+    file_path="oil-gas-sites.csv",
+    url="https://docs.google.com/spreadsheets/d/1vET6DVXo3K9MeMYJj9sksSTmQjV3v9JmIPSlR6HS4NA",
+    fetch=fetch_oil_gas_sites_csv,
     parse=parse_oil_gas_sites_csv,
 )
