2cc71d82d6452a7de91cd73b97e07b371ccda299
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2026-05-06 19:03:07 +1000

    Update livestock sector implementation to use headcounts dataset
    Resolves #182

--- src/openmethane_prior/sectors/livestock/sector.py
+++ src/openmethane_prior/sectors/livestock/sector.py
@@ -15,8 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 import numpy as np
+import pandas as pd
 import xarray as xr
 
 from openmethane_prior.lib import (
@@ -26,13 +26,23 @@ from openmethane_prior.lib import (
     PriorSector,
     PriorSectorConfig,
 )
+from openmethane_prior.lib.data_manager.parsers import parse_csv
 
 logger = logger.get_logger(__name__)
 
-livestock_data_source = DataSource(
-    name="enteric-fermentation",
-    url="https://openmethane.s3.amazonaws.com/prior/inputs/EntericFermentation.nc",
+livestock_headcount_data_source = DataSource(
+    name="livestock-headcount",
+    url="https://openmethane.s3.amazonaws.com/prior/inputs/lmap_selected_cols_Nasimeh_171025.csv",
+    parse=parse_csv,
 )
+
+
+def livestock_to_ch4_kg_year(beef: float, sheep: float, dairy: float) -> float:
+    """Calculate the annual emissions created by a number of beef cattle, dairy
+    cattle and/or sheep."""
+    # TODO: document where these emission factors come from
+    return beef * 51.0 + sheep * 6.8 + dairy * 93.0
+
 
 def process_emissions(
     sector: PriorSector,
@@ -40,33 +50,34 @@ def process_emissions(
     prior_ds: xr.Dataset,
 ):
     config = sector_config.prior_config
-
-    livestock_asset = sector_config.data_manager.get_asset(livestock_data_source)
-    with xr.open_dataset(livestock_asset.path) as lss:
-        ls = lss.load()
-
     domain_grid = config.domain().grid
 
-    # Re-project into domain coordinates
-    # - create meshgrids of the lats and lons
-    lonmesh, latmesh = np.meshgrid(ls.lon, ls.lat)
-    cell_x, cell_y, cell_valid = domain_grid.lonlat_to_cell_index(lonmesh, latmesh)
+    livestock_headcount_da = sector_config.data_manager.get_asset(livestock_headcount_data_source)
+    livestock_df: pd.DataFrame = livestock_headcount_da.data
+    livestock_df = livestock_df.rename(columns={"X": "lon", "Y": "lat"})
 
-    enteric_as_array = lss.CH4_total.to_numpy()
+    # find the domain grid cell (ix, iy) each livestock area centre point is in
+    livestock_df["ix"], livestock_df["iy"], valid_cells = domain_grid.lonlat_to_cell_index(
+        lon=livestock_df["lon"],
+        lat=livestock_df["lat"],
+    )
 
-    livestockCH4 = np.zeros(domain_grid.shape)
-    logger.info("Distribute livestock CH4 (long process)")
-    # we're accumulating emissions from fine to coarse grid
-    # accumulate in mass units and divide by area at end
-    for j in range(ls.lat.size):
-        ix, iy = cell_x[j,:], cell_y[j,:]
-        # input domain is bigger so mask indices out of range
-        mask = cell_valid[j, :]
-        if mask.any():
-            # the following needs to use .at method since iy,ix indices may be repeated and we need to acumulate
-            np.add.at(livestockCH4, (iy[mask], ix[mask]), enteric_as_array[j, mask])
+    # throw away any data outside the domain
+    livestock_df = livestock_df[valid_cells]
 
-    return convert_to_timescale(livestockCH4, domain_grid.cell_area)
+    ch4_gridded = np.zeros(domain_grid.shape)
+    for _, livestock_area in livestock_df.iterrows():
+        # calculate the total annual CH4 emissions for this area
+        livestock_ch4 = livestock_to_ch4_kg_year(
+            beef=livestock_area["heads_mapped_mix_beef"],
+            sheep=livestock_area["heads_mapped_mix_sheep"],
+            dairy=livestock_area["heads_mapped_dairy"],
+        )
+        # add the CH4 to the grid
+        ch4_gridded[livestock_area["iy"], livestock_area["ix"]] += livestock_ch4
+
+    # convert annual CH4 kg to CH4 kg/s/m3
+    return convert_to_timescale(ch4_gridded, domain_grid.cell_area)
 
 
 sector = PriorSector(
